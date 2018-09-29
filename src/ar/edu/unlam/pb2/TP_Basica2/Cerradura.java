98ac25b948cff57b1a3ff96413a891862ca6a1e0
Pablo Teruel <teruelpablo@gmail.com>
2018-09-29 20:29:53 -0300

    Termine el ejercicio, no tengo idea si esta bien y me costo un monton, estoy al horno :(.

--- src/ar/edu/unlam/pb2/TP_Basica2/Cerradura.java
+++ src/ar/edu/unlam/pb2/TP_Basica2/Cerradura.java
@@ -1,10 +1,67 @@
-package ar.edu.unlam.pb2;
+package ar.edu.unlam.pb2.TP_Basica2;
 
 public class Cerradura {
 	
 	//Atributos
+	private Integer claveDeApertura, cantidadDeFallosConsecutivosQueLaBloquean;
+	private Boolean estadoCerradura;
+	private Integer aperturasExitosas, aperturasFallidas;
 	
 	//Constructor
+	public Cerradura (Integer claveDeApertura, Integer cantidadDeFallosConsecutivosQueLaBloquean) {
+		this.claveDeApertura = claveDeApertura;
+		this.cantidadDeFallosConsecutivosQueLaBloquean = cantidadDeFallosConsecutivosQueLaBloquean;
+		this.estadoCerradura = false;
+		this.aperturasExitosas = 0;
+		this.aperturasFallidas = 0;
+	}
 	
 	//Metodos
+	public Boolean abrir(Integer clave) {
+		if(this.claveDeApertura == clave) {
+			this.estadoCerradura = true;
+			this.aperturasExitosas+=1;
+			this.aperturasFallidas=0;
+		} else {
+			this.aperturasFallidas+=1;
+		}
+		return this.estadoCerradura;
+	}
+	
+	public void cerrar() {
+		this.estadoCerradura = false;
+	}
+	
+	public Boolean estaAbierta() {
+		if(this.estadoCerradura==true) {
+			return true;
+		} else {
+		return false;
+		}
+	}
+	
+	public Boolean estaCerrada() {
+		if(this.estadoCerradura==false) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public Boolean fueBloqueada() {
+		if(this.cantidadDeFallosConsecutivosQueLaBloquean >= this.aperturasFallidas) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public Integer contarAperturasExitosas() {
+		return this.aperturasExitosas;
+	}
+	
+	public Integer contarAperturasFallidas() {
+		return this.aperturasFallidas;
+	}
+	
 }
